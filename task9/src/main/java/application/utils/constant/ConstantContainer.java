07b39ff902d8ef2a995c1fa5de6d4805df3c3b82
Ivan Pavliushchyk <131768267+Ivan-3-14@users.noreply.github.com>
2024-02-21 06:51:51 +0300

    JOIN_ONE_TO_MANY

--- task9/src/main/java/application/utils/constant/ConstantContainer.java
+++ task9/src/main/java/application/utils/constant/ConstantContainer.java
@@ -2,7 +2,6 @@ package application.utils.constant;
 
 public class ConstantContainer {
 
-@SuppressWarnings("checkstyle:StaticVariableName")
 public  static String SELECT_ALL_PEOPLE_FROM_DB = "select p from People p";
 
     private ConstantContainer() {
